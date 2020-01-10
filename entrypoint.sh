cb203a469f13521456d2c0f1224deed4244cb8a4
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2020-01-10 16:58:48 +0100

    Exits if the event wasn't about a milestone

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -u
 
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
+  echo "::debug::The event name was '$GITHUB_EVENT_NAME'"
+  exit 0
+fi
+
 echo "::set-output name=release-url::https://example.com"
 
 exit 0
