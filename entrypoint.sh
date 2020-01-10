354da4e7ee17ede8273c51721cb79051c5c10483
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2020-01-10 16:58:48 +0100

    Exits if the event wasn't about a milestone

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,13 @@ if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
   exit 0
 fi
 
+event_type=$(jq .action $GITHUB_EVENT_PATH)
+
+if [ $event_type != "closed" ]; then
+  echo "::debug::The event type was '$event_type'"
+  exit 0
+fi
+
 echo "::set-output name=release-url::https://example.com"
 
 exit 0
