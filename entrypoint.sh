bef383d7959bd924b678745280ac4a901ed0d27b
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2020-01-13 11:52:39 +0100

    Removes the double quotes from the parsed JSON values

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,14 @@ if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
   exit 0
 fi
 
-event_type=$(jq .action $GITHUB_EVENT_PATH)
+event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
 
 if [ $event_type != "closed" ]; then
   echo "::debug::The event type was '$event_type'"
   exit 0
 fi
 
-milestone_name=$(jq .milestone.title $GITHUB_EVENT_PATH)
+milestone_name=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
 
 # Splits the string in $GITHUB_REPOSITORY which takes
 # the form "owner/repository" into two variables
